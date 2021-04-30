c9571618888ed68f24c27d2ab22c9c5eca972996
WhiteArce <whiteheim.09@gmail.com>
2021-04-29 17:30:37 -0700

    Estilos
    Mas integración de estilos

--- core/chat.js
+++ core/chat.js
@@ -1,36 +1,40 @@
 const socket = io();
 
 // Elements
-let message = document.getElementById('message');
-let username = document.getElementById('username');
-let buttonSend = document.getElementById('send-button');
-let messageWindow = document.getElementById('messages-window');
-let actions = document.getElementById('actions');
+let message = document.getElementById("message");
+let username = document.getElementById("username");
+let buttonSend = document.getElementById("send-button");
+let messageWindow = document.getElementById("messages-window");
+let actions = document.getElementById("actions");
 
-buttonSend.addEventListener('click', () => {
-    if (username.value == '' || message.value == '') {
+
+// Validation for the messages
+buttonSend.addEventListener("click", () => {
+    if (username.value == "" || message.value == "") {
         alert("Please insert the username or a message instead");
-    }else{
-    socket.emit('chat-message', {
+    } else {
+        sendMessage(username, message);
+    }
+});
+
+const sendMessage = (username, message) => {
+    socket.emit("chat-message", {
         username: username.value,
-        message: message.value
+        message: message.value,
     });
 }
-});
 
-socket.on('chat-message', data => {
-    actions.innerHTML = '',
-    messageWindow.innerHTML += `<p>
+socket.on("chat-message", (data) => {
+    (actions.innerHTML = ""),
+        (messageWindow.innerHTML += `<p>
     <strong>${data.username}</strong>: ${data.message}
-    </p>`
+    </p>`);
 });
 
-
-message.addEventListener('keypress', () => {
-    socket.emit('chat-typing', username.value);
+message.addEventListener("keypress", () => {
+    socket.emit("chat-typing", username.value);
 });
 
-socket.on('chat-typing', data => {
+socket.on("chat-typing", (data) => {
     actions.innerHTML = `<p> <em>${data} it's typing...</em></p>`;
 });
-
